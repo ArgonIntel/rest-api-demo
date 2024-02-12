fc4bd481def7d6ac1a309dcc4ef47207e79219ea
Goran Perisic <gperisic@outlook.com>
2024-02-12 17:04:55 +0100

    finished version

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import json
+
 from flask import Flask
+from flask.json import jsonify
+
+from dataset import get_country_data
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello_world():
-    return "<h1>Hello World</h1>"
+
+@app.route("/api")
+def country_data():
+    data_df = get_country_data()
+    data_dict = json.loads(data_df.to_json())
+    return jsonify(data_dict)
+
+@app.route("/api/<country>")
+def country_specific_data(country):
+    data_df = get_country_data()
+    data_dict = json.loads(data_df.to_json())
+
+    country_data = data_dict.get(country.lower(), {})
+    
+    return jsonify(country_data)
